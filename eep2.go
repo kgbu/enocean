df4759fcc46e8712c9cb58f661d73b74c5121b95
OCAO Kazutaka Ogaki <ocaokgbu@gmail.com>
2015-10-22 16:29:04 +0900

    initiate eep2.go

---
+++ eep2.go
@@ -0,0 +1,21 @@
+package enocean
+
+// type of EnOcean Radio Protocol2 Raw Data packet in EnOcean Serial Protocol3
+
+type ERP2Payload struct {
+	PayloadType byte
+	Identifier  []byte
+	Data        []byte
+	CRC8        byte
+}
+
+type ERP2inESP3 struct {
+	ERP2Header       byte
+	ERP2ExtendHeader []byte
+	Payload          []byte
+}
+
+type ERP2 struct {
+	Length byte
+	Body   EEP2inESP3
+}
